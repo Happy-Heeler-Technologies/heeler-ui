6a9cd0836835d1129f58044c1977a734e7ed7742
Brian Lindquist <brian.p.lindquist@gmail.com>
2025-11-29 16:06:00 -0600

    add Loading component

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./components/Button/Button";
 export * from "./components/Card/Card";
 export * from "./components/Checkbox/Checkbox";
 export * from "./components/Input/Input";
+export * from "./components/Loading/Loading";
 export * from "./components/Tabs/Tabs";
 
 // Icons
